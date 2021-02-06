34f8ae98f5b65f79c2de97cb8d37d24ce7a569c6
darshilagrawal <darshilagrawal2000@gmail.com>
2021-02-06 11:12:47 +0530

    a SwiftUi Implementation of Static Photo App

---
+++ I Am Rich(Swift UI)/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  I Am Rich(Swift UI)
+//
+//  Created by Darshil Agrawal on 31/07/20.
+//  Copyright © 2020 Darshil Agrawal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(.systemTeal).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                .font(.system(size: 40))
+                .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond").resizable().aspectRatio(contentMode: .fit).frame(width: 200.0, height: 200.0,alignment: .center)
+            }
+        }
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
